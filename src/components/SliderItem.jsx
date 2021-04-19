ca000da46e4c61b9e5540b3c311f0000d3a60ece
Shamany4 <shamany4@Shamany4s-iMac.local>
2021-04-20 03:13:48 +0700

    added support SVG image and made slideritem component

--- src/components/SliderItem.jsx
+++ src/components/SliderItem.jsx
@@ -1,29 +1,61 @@
 import React from "react";
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Icon from "../icons/position.svg";
+import TypeIcon from "../buildings/shopping.svg";
 
 export default function SliderItem() {
   return (
     <View style={styles.sliderItem}>
       <View style={styles.sliderInfo}>
         <View style={styles.sliderLocation}>
-
+          <Icon width={25} height={25} style={styles.sliderLocation__icon}/>
+          <Text style={styles.sliderLocation__text}>2.68 км</Text>
         </View>
+        <Text style={styles.sliderTitle}>Континент</Text>
+        <Text style={styles.sliderSubtitle}>Торговый центр</Text>
       </View>
+      <TypeIcon width={60} height={60}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   sliderItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
     height: 150,
     width: '100%',
     backgroundColor: '#f8d57e',
     borderRadius: 10,
+    padding: 20
   },
   sliderInfo: {
-
+    flex: 1,
+    justifyContent: 'center'
   },
   sliderLocation: {
-
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15
+  },
+  sliderLocation__icon: {
+    marginRight: 5
+  },
+  sliderLocation__text: {
+    fontFamily: 'Gilroy-Regular',
+    fontSize: 12,
+    letterSpacing: 0.2,
+  },
+  sliderTitle: {
+    fontFamily: 'Gilroy-ExtraBold',
+    fontSize: 25,
+    letterSpacing: 0.1,
+  },
+  sliderSubtitle: {
+    fontFamily: 'Gilroy-Regular',
+    fontSize: 14,
+    letterSpacing: 0.24,
   }
 })
